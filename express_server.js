c923d9da42f92feca8c55c10e84b19d147e2e111
Sebastien <perigon@hotmail.com>
2021-01-18 18:40:22 +0000

    route and view created for urls_show

--- express_server.js
+++ express_server.js
@@ -16,6 +16,7 @@ app.get("/", (req, res) => {
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
@@ -25,6 +26,11 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World"}
   res.render("hello_world", templateVars)
 })
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
