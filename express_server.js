bd075806a2ae9ebf0b9414e50cb7cf049e0877d7
Sebastien <perigon@hotmail.com>
2021-01-18 19:47:33 +0000

    saving new site to urlDatabase working

--- express_server.js
+++ express_server.js
@@ -14,7 +14,7 @@ const generateRandomString = () => {
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
-    "9sm5xK": "http://www.google.com"
+    "Fsm5xK": "http://www.google.com",
 };
 
 app.get("/", (req, res) => {
@@ -26,23 +26,29 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
+  let newIndex = generateRandomString();
+  urlDatabase[newIndex] = req.body.longURL
+
   console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  res.redirect("/urls")       // Respond with 'Ok' (we will replace this)
 });
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
 
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls", (req, res) => {
-  const templateVars = {urls: urlDatabase};
-  res.render("urls_index", templateVars);
-});
+
 
 app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World"}
