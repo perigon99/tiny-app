c8c4c367e5a03d95796ea6030960af9abd5b2306
Sebastien <perigon@hotmail.com>
2021-01-18 19:25:02 +0000

    random string generator

--- express_server.js
+++ express_server.js
@@ -2,7 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.set("view engine", "ejs");
+
+const generateRandomString = () => {
+ return Math.random().toString(36).substring(7)
+}
+
 
 const urlDatabase = {
     "b2xVn2": "http://www.lighthouselabs.ca",
@@ -17,6 +25,20 @@ app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = {urls: urlDatabase};
   res.render("urls_index", templateVars);
@@ -27,14 +49,9 @@ app.get("/hello", (req, res) => {
   res.render("hello_world", templateVars)
 })
 
-app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
-});
-
-app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  res.render("urls_show", templateVars);
-});
+app.get("*", (req, res) => {
+  res.send("404 page not found")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`);
